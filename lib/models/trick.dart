84145d97bb0dfd3ffbb7463d773fbae3c6ee0d68
Poonyaritkwangthong <117970215+Poonyaritkwangthong@users.noreply.github.com>
2023-08-20 08:34:51 +0700

    Add files via upload

---
+++ lib/models/trick.dart
@@ -0,0 +1,25 @@
+import 'shoe.dart';
+
+class Cart {
+  // list of champion for sale
+  List<Shoe> shoeShop = [
+    Shoe(
+        name: 'veigar',
+        detail: 'yordle',
+        description:
+            'Veigar has embraced powers that few others dare even approach',
+        imagePath: 'lib/images/veigar.png'),
+    Shoe(
+        name: 'zet',
+        detail: detail,
+        description: description,
+        imagePath: imagePath),
+  ];
+  // list of items in user cart
+
+  // get list of trick
+
+  // add items to champion
+
+  // remove item from champion
+}
